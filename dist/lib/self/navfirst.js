60272aa9d148801c6572c23528db28f98511fccb
gujianteng <541051981@qq.com>
2021-01-16 16:05:50 +0800

    完成三角形app
    完成上滑下滑效果，实现小三角形

--- dist/lib/self/navfirst.js
+++ dist/lib/self/navfirst.js
@@ -10,8 +10,32 @@ var navf = function () {
                 res.forEach(item => {
                     str += `<li>${ item.name }</li>`
                   })
-                  console.log(str);
+                //   console.log(str);
                   $('.gu').html(str)
+                //   第20个li创建三角型这个元素
+                  $('.gu > li').eq(20)
+                  .append("<div class='sanjiaoapp'><div class='sanjiao'></div><div class='sanjiaoimg'><img src='../../images/sanjiaoimg.jpg'><div class='appzi'>小米商城APP</div></div></div>")
+                // 给第20个li添加类名
+                $('.gu > li').eq(20).addClass('sanjiaoli')
+
+                  $('.gu > li').eq(20).on('mouseover', function(){
+                    $(".sanjiaoli>.sanjiaoapp>.sanjiao").css({
+                        display:'block'
+                    })
+                    // 移入的时候app图片下滑
+                    $('.sanjiaoimg').stop().slideDown(200, 'linear', function () {
+                        console.log('下滑结束')
+                      })
+                  })
+                  $('.gu > li').eq(20).on('mouseout', function(){
+                    $(".sanjiaoli>.sanjiaoapp>.sanjiao").css({
+                        display:'none'
+                    })
+                    // 鼠标一开上滑
+                    $('.sanjiaoimg').stop().slideUp(200, 'linear', function () {
+                        console.log('下滑结束')
+                      })
+                  })
 
             }
         })
