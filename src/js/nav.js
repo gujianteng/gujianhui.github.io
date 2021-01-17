675dd7cf684e1b6e5ca8d1e3083c85afe3ee335f
gujianteng <541051981@qq.com>
2021-01-17 18:45:00 +0800

    重新修改
    除了没有两张图片的效果，其他完美

---
+++ src/js/nav.js
@@ -0,0 +1,77 @@
+
+
+
+$.ajax({
+    url: '../lib/navfirst.json',
+    dataType: 'json',
+    success: function (res) {
+      // console.log(res);
+      var str = ''
+      res.forEach(item => {
+        str += `<li>${item.name}</li>`
+      })
+      //   console.log(str);
+      $('.gu').html(str)
+      //   第20个li创建三角型这个元素
+      $('.gu > li').eq(20)
+        .append("<div class='sanjiaoapp'><div class='sanjiao'></div><div class='sanjiaoimg'><img src='../images/sanjiaoimg.jpg'><div class='appzi'>小米商城APP</div></div></div>")
+      // 给第20个li添加类名
+      $('.gu > li').eq(20).addClass('sanjiaoli')
+  
+      $('.gu > li').eq(20).on('mouseover', function () {
+        $(".sanjiaoli>.sanjiaoapp>.sanjiao").css({
+          display: 'block'
+        })
+        // 移入的时候app图片下滑
+        $('.sanjiaoimg').stop().slideDown(200, 'linear', function () {
+          // console.log('下滑结束')
+        })
+      })
+      $('.gu > li').eq(20).on('mouseout', function () {
+        $(".sanjiaoli>.sanjiaoapp>.sanjiao").css({
+          display: 'none'
+        })
+        // 鼠标一开上滑
+        $('.sanjiaoimg').stop().slideUp(200, 'linear', function () {
+          // console.log('下滑结束')
+        })
+      })
+  
+    }
+  })
+  
+  
+  
+  
+  $('.card').on('mouseover', function () {
+    $(".iconliang ").css({
+      background: 'white'
+    })
+    $('.iconright').css({
+      color: 'red'
+    })
+    $('.icon').css({
+      color: 'red'
+    })
+    $('.cardxiahua').stop().slideDown(200, 'linear', function () { })
+  
+  })
+  
+  
+  
+  $('.card').on('mouseout', function () {
+    $(".iconliang ").css({
+      background: '#424242'
+    })
+    $('.iconright').css({
+      color: '#b0b0b0'
+    })
+    $('.icon').css({
+      color: '#b0b0b0'
+    })
+  
+    $('.cardxiahua').stop().slideUp(200, 'linear', function () { })
+  })
+  
+  
+  
--- src/lib/self/min.js
+++
@@ -1,7 +0,0 @@
-import moduleA from './fengzhuangswiper.js'
-
-import moduleB from './navfirst.js'
-
-
-moduleA.fengswiper()
-moduleB.navf()
