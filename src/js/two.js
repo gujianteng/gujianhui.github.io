aacfab54652fae449b2c0c702eb0b73c9aa40ae9
gujianteng <541051981@qq.com>
2021-01-26 22:38:12 +0800

    完成轮播图

---
+++ src/js/two.js
@@ -0,0 +1,33 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    // direction: 'vertical', // 垂直切换选项
+    loop: true, // 循环模式选项
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    effect : 'fade',
+
+    // 如果需要分页器
+    pagination: {
+      el: '.swiper-pagination',
+      clickable :true,
+    },
+
+    // 如果需要前进后退按钮
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // 如果需要滚动条
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  })
+  $('.swiper-container').on('mouseenter',function(){
+    mySwiper.autoplay.stop();
+  })
+  $('.swiper-container').on('mouseleave',function(){
+    mySwiper.autoplay.start();
+  })
+
