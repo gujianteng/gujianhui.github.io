f8ed101bfcc0045cdf3aab4dd3b53f1324e38c06
gujianteng <541051981@qq.com>
2021-02-01 22:02:24 +0800

    完成家电块布局

---
+++ src/js/three-jiadian.js
@@ -0,0 +1,46 @@
+$('.home-brick-box-jiadian>.box-hd>.more>ul>.tab-active').css({
+    color: "#ff6700",
+    borderBottom: "2px solid #ff6700"
+})
+
+$('.home-brick-box-jiadian>.box-bd>.row>.span16>div').eq(0).children().eq(0).children().last('div')
+console.log($('.home-brick-box-jiadian>.box-bd>.row>.span16>div').eq(0).children().eq(0).children().eq(7).children().eq(0));
+
+$('.home-brick-box-jiadian>.box-bd>.row>.span16>div').eq(0).children().eq(0).children().eq(7).children().eq(0).css({
+    height: "143",
+    width: "234",
+    paddingTop: 50,
+    background: "#fff",
+    position: "relative",
+    zIndex: 1,
+    float: "left",
+})
+
+$('.home-brick-box-jiadian>.box-bd>.row>.span16>div').eq(0).children().eq(0).children().eq(7).children().eq(0).children().eq(0).css({
+    position:" absolute",
+    right: 20,
+    top: 32,
+    width: 80,
+    height: 80
+})
+
+
+$('.home-brick-box-jiadian>.box-bd>.row>.span16>div').eq(0).children().eq(0).children().eq(7).children().eq(0).children().eq(1).css({
+    margin:" -10px 110px 5px 30px",
+    fontSize: 14,
+    fontWeight: 400,
+    width: 94,
+    height: 63,
+    // display: "block",
+    whiteSpace:"normal",
+    textAlign: "left"
+
+})
+
+
+
+$('.home-brick-box-jiadian>.box-bd>.row>.span16>div').eq(0).children().eq(0).children().eq(7).children().eq(0).children().eq(2).css({
+    textAlign: "left",
+    paddingLeft: 17
+    
+})
